e728479f6867dcd67fb263b61b1c560f64fca35e
basalaev <basalaev.dev@gmail.com>
2019-11-23 17:30:30 +0300

    Add lesson 1 project

---
+++ lesson1/lesson1/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  lesson1
+//
+//  Created by basalaev on 23.11.2019.
+//  Copyright © 2019 Heads and Hands. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ lesson1/lesson1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson1
+//
+//  Created by basalaev on 23.11.2019.
+//  Copyright © 2019 Heads and Hands. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
